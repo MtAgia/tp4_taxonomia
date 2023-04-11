fec2325990829526f3d5ce5177405de590a4f47a
Matias G <dikimatig@gmail.com>
2023-04-11 03:31:08 -0300

    terminando get setters y funcion btn

--- js/app.js
+++ js/app.js
@@ -13,19 +13,85 @@ las propiedades mediante un formulario, también agregar los botones “mostrar 
 indicar en un alert el resultado de la función correspondiente.
 */
 
+let btnSubir = document.getElementById(`btnSubir`).addEventListener(`click`, subirElementos);
+class Persona {
+  //privadas
+  #nombre;
+  #dni;
+  #edad;
+  #sexo;
+  #peso;
+  #altura;
+  #anioNacimiento;
+  constructor(nombre, dni, edad, sexo, peso, altura, anioNacimiento) {
+    this.#nombre = nombre;
+    this.#dni = dni;
+    this.#edad = edad;
+    this.#sexo = sexo;
+    this.#peso = peso;
+    this.#altura = altura;
+    this.#anioNacimiento = anioNacimiento;
+  }
+  get nombres() {
+    return this.#nombre;
+  }
+  set nombres(nuevoNombre) {
+    this.#nombre = nuevoNombre;
+  }
+  get dnis(){
+    return this.#dni;
+  }
+  set dnis(nuevoDni){
+    this.#dni = nuevoDni
+  }
+  get edades(){
+    return this.#edad
+  }
+  set edades(nuevaEdad){
+    this.#edad = nuevaEdad
+  }
+  get sexos(){
+    return this.#sexo
+  }
+  set sexos(nuevoSexo){
+    this.#sexo = nuevoSexo
+  }
+  get pesos(){
+    return this.#sexo
+  }
+  set pesos(nuevoPeso){
+    this.#peso = nuevoPeso
+  }
+  get alturas(){
+    return this.#altura
+  }
+  set alturas(nuevaAltura){
+    this.#altura = nuevaAltura
+  }
+  get anioNacimientos(){
+    return this.#anioNacimiento
+  }
+  set anioNacimientos(nuevoAnio){
+    this.#anioNacimiento = nuevoAnio
+  }
+}
 
 
-
-
-class Persona{
-    #nombre
-    #dni
-    #edad
-    #sexo
-    #peso
-    #altura
-    #anioNacimiento
-    constructor (){
-
+function subirElementos() {
+    let inputNombre = document.getElementById(`nombre`);
+    let inputDni = document.getElementById(`dni`);
+    let inputEdad = document.getElementById(`edad`);
+    let inputHombre = document.getElementById(`hombre`).checked
+    let inputMujer = document.getElementById(`mujer`).checked
+    let sexo = ""
+    if (inputHombre === true){
+        sexo = "hombre"
+    } else if(inputMujer === true){
+        sexo = "mujer"
     }
+    let inputPeso = document.getElementById(`peso`);
+    let inputAltura = document.getElementById(`altura`);
+    let inputAnio = document.getElementById(`nacimiento`);
+    let personas = new Persona(inputNombre.value , inputDni.value, inputEdad.value, sexo, inputPeso.value, inputAltura.value, inputAnio.value);
+    console.log(personas)
 }
